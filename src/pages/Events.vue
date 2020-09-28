125c35ab706950aa56fbe06bbb8db3ed67fe80e7
Chris Rault <chris@smokeyfro.com>
2020-09-28 17:59:21 +0200

    Added the event image

--- src/pages/Events.vue
+++ src/pages/Events.vue
@@ -8,7 +8,7 @@
         <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 w-full mr-0">
             <div class="bg-gray-900 p-6 rounded-md shadow-md flex flex-col justify-between" v-for="item in $page.posts.edges" :key="item.node.id">
                 <div>
-                    <figure>
+                    <figure class="overflow-hidden rounded-md">
                         <g-image :src="$page.post.image"></g-image>
                     </figure>
                     <h3 class="text-3xl text-white leading-9 font-black">{{ item.node.title }}</h3>
