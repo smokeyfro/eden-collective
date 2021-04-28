df310f43e981ccec296d042e26c4ddabb6299059
Chris Rault <chris@smokeyfro.com>
2021-04-28 11:12:56 +0200

    Testing the comments

--- src/templates/JournalPost.vue
+++ src/templates/JournalPost.vue
@@ -1,26 +1,34 @@
 <template>
-<Layout>
+  <Layout>
     <div class="journal prose lg:prose-xl">
-        <div class="journal-header">
-            <h1 v-html="$page.post.title" class="journal-title" />
-            <div class="journal-meta">
-                <div class="journal-author">
-                    <span class="label">Author</span>
-                    <span class="author-name" v-text="$page.post.author" />
-                </div>
-                <div class="journal-date">
-                    <span class="label">Date</span>
-                    <div v-text="$page.post.date" />
-                </div>
-                <div class="journal-time">
-                    <span class="label">Time</span>
-                    <span>{{ $page.post.timeToRead }} min read</span>
-                </div>
-            </div>
+      <div class="journal-header">
+        <h1 v-html="$page.post.title" class="journal-title" />
+        <div class="journal-meta">
+          <div class="journal-author">
+            <span class="label">Author</span>
+            <span class="author-name" v-text="$page.post.author" />
+          </div>
+          <div class="journal-date">
+            <span class="label">Date</span>
+            <div v-text="$page.post.date" />
+          </div>
+          <div class="journal-time">
+            <span class="label">Time</span>
+            <span>{{ $page.post.timeToRead }} min read</span>
+          </div>
         </div>
-        <JournalContent :content="$page.post.content" />
+      </div>
+      <JournalContent :content="$page.post.content" />
+      <div
+        id="cusdis_thread"
+        data-host="https://sfcomments.vercel.app"
+        data-app-id="670bd0b4-0b48-4f4e-ae75-73db184557e9"
+        :data-page-id="$page.post.id"
+        :data-page-url="this.route"
+        :data-page-title="$page.post.title"
+      ></div>
     </div>
-</Layout>
+  </Layout>
 </template>
 
 <page-query>
@@ -36,46 +44,54 @@ query JournalPost ($path: String!) {
 </page-query>
 
 <script>
-import JournalContent from "@/components/JournalContent"
+import JournalContent from "@/components/JournalContent";
 
 export default {
-    components: {
-        JournalContent
-    },
-    metaInfo() {
-        return {
-            title: this.$page.post.title
-        }
-    }
-}
+  components: {
+    JournalContent,
+  },
+  metaInfo() {
+    return {
+      title: this.$page.post.title,
+      script: [
+        {
+          src: "https://sfcomments.vercel.app/js/cusdis.es.js",
+          async: true,
+          defer: true,
+          type: "text/javascript",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style scoped>
 .journal-container {
-    max-width: 840px;
+  max-width: 840px;
 }
 
 .journal-header {
-    padding: 2rem 0 4rem 0;
+  padding: 2rem 0 4rem 0;
 }
 
 .journal-title {
-    font-size: 4rem;
-    margin: 0 0 4rem 0;
-    padding: 0;
+  font-size: 4rem;
+  margin: 0 0 4rem 0;
+  padding: 0;
 }
 
 .journal-meta {
-    display: flex;
-    flex-wrap: wrap;
-    font-size: 0.8rem;
+  display: flex;
+  flex-wrap: wrap;
+  font-size: 0.8rem;
 }
 
-.journal-meta>div {
-    margin-right: 4rem;
+.journal-meta > div {
+  margin-right: 4rem;
 }
 
-.journal-meta>div:last-of-type {
-    margin: 0;
+.journal-meta > div:last-of-type {
+  margin: 0;
 }
 </style>
