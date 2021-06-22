49d8ca1eb8053b223d142fe3981afa61aea238e7
Chris Rault <chris@smokeyfro.com>
2021-06-22 16:36:13 +0200

    Updated the about nav

--- src/components/AboutNav.vue
+++ src/components/AboutNav.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="bg-gray-100 p-10 flex flex-col space-y-5 ml-10 mt-20">
     <g-link to="/about" exact>About Us</g-link>
-    <g-link to="/about/action-plan" exact>The ACTion Plan</g-link>
-    <g-link to="/about/meet-the-community" exact>Meet the Community</g-link>
-    <g-link to="/about/food-security" exact>Food Security</g-link>
-    <g-link to="/about/how-to-join" exact>How to Join</g-link>
+    <g-link to="/action-plan" exact>The ACTion Plan</g-link>
+    <g-link to="/meet-the-community" exact>Meet the Community</g-link>
+    <g-link to="/food-security" exact>Food Security</g-link>
+    <g-link to="/join" exact>How to Join</g-link>
   </nav>
 </template>
