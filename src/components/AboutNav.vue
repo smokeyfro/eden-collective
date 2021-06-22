eedb05f0cf7d1c59d27809c6ecc16a8e1e3b2fa7
Chris Rault <chris@smokeyfro.com>
2021-06-22 17:12:40 +0200

    About nav

--- src/components/AboutNav.vue
+++ src/components/AboutNav.vue
@@ -1,5 +1,7 @@
 <template>
-  <nav class="bg-gray-100 p-10 sticky top-0 flex flex-col space-y-5 w-full lg:ml-10">
+  <nav
+    class="bg-gray-100 p-10 sticky top-0 flex flex-col space-y-5 w-full lg:w-1/5 lg:ml-10"
+  >
     <g-link to="/about" exact>About Us</g-link>
     <g-link to="/action-plan" exact>The Action Plan</g-link>
     <g-link to="/meet-the-community" exact>Meet the Community</g-link>
