364cb2b1b40c3d94ffccecef4a3070c43b390e9c
Chris Rault <chris@smokeyfro.com>
2021-06-22 16:53:06 +0200

    About nav

--- src/components/AboutNav.vue
+++ src/components/AboutNav.vue
@@ -1,7 +1,7 @@
 <template>
   <nav class="bg-gray-100 p-10 sticky top-0 flex flex-col space-y-5 ml-10">
     <g-link to="/about" exact>About Us</g-link>
-    <g-link to="/action-plan" exact>The ACTion Plan</g-link>
+    <g-link to="/action-plan" exact>The Action Plan</g-link>
     <g-link to="/meet-the-community" exact>Meet the Community</g-link>
     <g-link to="/food-security" exact>Food Security</g-link>
     <g-link to="/join" exact>How to Join</g-link>
